8e5b3815f6a2147bc5709de65152db48659bcf70
Anubis <correaanubis@gmail.com>
2024-04-10 01:09:16 -0500

    new agent 6

--- script.groovy
+++ script.groovy
@@ -1,13 +1,13 @@
 def buildApp(){
-    echo 'building app from a groovy script 2'
+    echo 'building app from a groovy script 3'
 }
 
 def testApp(){
-    echo 'testing app from a groovy script 2'
+    echo 'testing app from a groovy script 3'
 }
 
 def deployApp(){
-    echo 'deploying app from a groovy script 2'
+    echo 'deploying app from a groovy script 3'
     echo "deploying version ${params.Version}"
 }
 
